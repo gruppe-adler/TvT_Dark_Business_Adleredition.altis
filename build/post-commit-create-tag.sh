283be40ea493aa1025a6e708a0abd1a0a4df0526
Moritz Schmidt <fusselwurm@gmail.com>
2016-07-29 22:52:09 +0200

    version bump, fixup travis build

--- build/post-commit-create-tag.sh
+++ build/post-commit-create-tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-package_json=`git rev-parse --show-toplevel`/package.json
-version=`git diff HEAD^..HEAD -- $package_json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
+package_json="`git rev-parse --show-toplevel`/package.json"
+version=`git diff HEAD^..HEAD -- "$package_json" | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
 
 if [ "$version" != "" ]; then
 	TAG=`node -e "console.log(JSON.parse(require('fs').readFileSync('$package_json')).version)"`
