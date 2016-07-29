9765d92a40b68edba8d176b5767a52b0a6586240
Moritz Schmidt <fusselwurm@gmail.com>
2016-07-29 23:00:52 +0200

    build fixes

--- build/build.sh
+++ build/build.sh
@@ -3,8 +3,8 @@
 currentdir=`dirname $0`
 
 
-SQF_VALIDATOR=$HOME/usr/bin/sqf_validator.py
-ARMAKE=$HOME/usr/bin/armake
+SQF_VALIDATOR="$HOME/usr/bin/sqf_validator.py"
+ARMAKE="$HOME/usr/bin/armake"
 TAG=$TRAVIS_TAG
 if [[ $TAG == "" ]]
 then
@@ -13,11 +13,11 @@ fi
 
 if [[ ! -f $SQF_VALIDATOR ]]
 then
-	$currentdir/install_sqf_validator.sh $SQF_VALIDATOR || exit 1
+	"$currentdir/install_sqf_validator.sh" $SQF_VALIDATOR || exit 1
 fi
 if [[ ! -f $ARMAKE ]]
 then
-	$currentdir/install_armake.sh $ARMAKE || exit 2
+	"$currentdir/install_armake.sh" $ARMAKE || exit 2
 fi
 
 python3 $SQF_VALIDATOR
--- build/install_armake.sh
+++ build/install_armake.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 target=$1
 targetdir=`dirname $target`
-mkdir -p $targetdir
+mkdir -p "$targetdir"
 pushd $HOME
 git clone https://github.com/KoffeinFlummi/armake && cd armake && make
-mv bin/armake $targetdir && chmod 755 $target
+mv bin/armake "$targetdir" && chmod 755 $target
 cd ..
 rm -rf armake
 popd
