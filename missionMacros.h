32f9ac1bdfb5d59ab29bf905580637645fc74f2e
Moritz Schmidt <fusselwurm@gmail.com>
2016-08-07 10:45:37 +0200

    add list macros for config, shrink loadout file

--- missionMacros.h
+++ missionMacros.h
@@ -1,2 +1,23 @@
 #define debugLog(msg) if (DEBUG_MODE) then { [msg, "GRAD_mission_debug", [true, true, true] ] call CBA_fnc_debug; }
 #define adminLog(msg) [msg, "GRAD_mission_admin", [DEBUG_MODE, true, false] ] call CBA_fnc_debug
+
+// brazenly stolen from 1tac
+#define LIST_2(var1) var1,var1
+#define LIST_3(var1) var1,var1,var1
+#define LIST_4(var1) var1,var1,var1,var1
+#define LIST_5(var1) var1,var1,var1,var1,var1
+#define LIST_6(var1) var1,var1,var1,var1,var1,var1
+#define LIST_7(var1) var1,var1,var1,var1,var1,var1,var1
+#define LIST_8(var1) var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_9(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_10(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_11(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_12(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_13(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_14(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_15(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_16(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_17(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_18(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_19(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
+#define LIST_20(var1) var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1,var1
