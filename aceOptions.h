a420caced1b5b2fe38b7eb95a1af44f068c231ab
Moritz Schmidt <fusselwurm@gmail.com>
2017-06-02 22:11:07 +0200

    remove spectator restrictions

--- aceOptions.h
+++ aceOptions.h
@@ -8,3 +8,24 @@ class ace_winddeflection_vehicleEnabled {
     typeName = "BOOL";
     force = 1;
 };
+
+class ace_spectator_filterSides {
+    value = 3;
+    typeName = "SCALAR";
+    force = 1;
+};
+class ace_spectator_filterUnits {
+    value = 0;
+    typeName = "SCALAR";
+    force = 1;
+};
+class ace_spectator_restrictModes {
+    value = 0;
+    typeName = "SCALAR";
+    force = 1;
+};
+class ace_spectator_restrictVisions {
+    value = 0;
+    typeName = "SCALAR";
+    force = 1;
+};
