1601c1ceb1bb6653475cd877f5ba69e8d757f16d
Moritz Schmidt <fusselwurm@posteo.de>
2017-06-08 21:27:02 +0200

    Create install_armake.sh

--- build/install_armake.sh
+++ build/install_armake.sh
@@ -3,8 +3,13 @@ target=$1
 targetdir=`dirname $target`
 mkdir -p "$targetdir"
 pushd $HOME
-git clone https://github.com/KoffeinFlummi/armake && cd armake && make
-mv bin/armake "$targetdir" && chmod 755 $target
-cd ..
-rm -rf armake
+# git clone https://github.com/KoffeinFlummi/armake && cd armake && make
+# mv bin/armake "$targetdir" && chmod 755 $target
+#rm -rf armake
+# cd ..
+
+wget https://github.com/KoffeinFlummi/armake/releases/download/v0.5.1/armake_v0.5.1.zip
+unzip armake_v0.5.1.zip
+mv armake_v0.5.1/armake "$targetdir" && chmod 755 $target
+
 popd
