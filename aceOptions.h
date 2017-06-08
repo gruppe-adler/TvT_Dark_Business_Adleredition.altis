932fb50febd0bf3105a9baa076d53c97880ce00e
Moritz Schmidt <fusselwurm@gmail.com>
2017-06-08 22:29:52 +0200

    fix #36  ace options

--- aceOptions.h
+++ aceOptions.h
@@ -1,31 +1,39 @@
-class ace_winddeflection_enabled {
-    value = 0;
-    typeName = "BOOL";
-    force = 1;
-};
-class ace_winddeflection_vehicleEnabled {
-    value = 0;
-    typeName = "BOOL";
-    force = 1;
-};
+class ACE_Settings {
+    class ace_winddeflection_enabled {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_winddeflection_vehicleEnabled {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
 
-class ace_spectator_filterSides {
-    value = 3;
-    typeName = "SCALAR";
-    force = 1;
-};
-class ace_spectator_filterUnits {
-    value = 0;
-    typeName = "SCALAR";
-    force = 1;
-};
-class ace_spectator_restrictModes {
-    value = 0;
-    typeName = "SCALAR";
-    force = 1;
-};
-class ace_spectator_restrictVisions {
-    value = 0;
-    typeName = "SCALAR";
-    force = 1;
+    class ace_spectator_filterSides {
+        value = 3;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_spectator_filterUnits {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_spectator_restrictModes {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_spectator_restrictVisions {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+
+    class ace_hearing_enableCombatDeafness {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
 };
