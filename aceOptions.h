0351501acedaa19e33b60bdfe946f643eebb5535
Moritz Schmidt <fusselwurm@gmail.com>
2017-06-18 22:03:17 +0200

    ACE medical settings

--- aceOptions.h
+++ aceOptions.h
@@ -36,4 +36,222 @@ class ACE_Settings {
         typeName = "BOOL";
         force = 1;
     };
+
+
+    // --- medical ---
+
+    class ace_medical_blood_enabledFor {
+        value = 2;
+        typeName = "SCALAR";
+        force = 1;
+    };
+
+    class ace_medical_level {
+        value = 2;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_medicSetting {
+        value = 2;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_increaseTrainingInLocations {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_enableFor {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_enableOverdosing {
+        value = 1;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_bleedingCoefficient {
+        value = 0.7;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_painCoefficient {
+        value = 1;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_enableAirway {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_enableFractures {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_enableAdvancedWounds {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_enableVehicleCrashes {
+        value = 1;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_enableScreams {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_playerDamageThreshold {
+        value = 1;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_preventInstaDeath {
+        value = 1;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_enableRevive {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_allowDeadBodyMovement {
+        value = 1;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_allowLitterCreation {
+        value = 1;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_litterSimulationDetail {
+        value = 3;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_litterCleanUpDelay {
+        value = 600;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_medicSetting_basicEpi {
+        value = 1;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_medicSetting_PAK {
+        value = 1;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_medicSetting_SurgicalKit {
+        value = 1;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_consumeItem_PAK {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_consumeItem_SurgicalKit {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_useLocation_basicEpi {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_useLocation_PAK {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_useLocation_SurgicalKit {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_useCondition_PAK {
+        value = 1;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_useCondition_SurgicalKit {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_keepLocalSettingsSynced {
+        value = 1;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_healHitPointAfterAdvBandage {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_painIsOnlySuppressed {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_painEffectType {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_allowUnconsciousAnimationOnTreatment {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_moveUnitsFromGroupOnUnconscious {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_menuTypeStyle {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_delayUnconCaptive {
+        value = 3;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_menu_allow {
+        value = 1;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_menu_useMenu {
+        value = 1;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_medical_menu_openAfterTreatment {
+        value = 1;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_medical_menu_maxRange {
+        value = 3;
+        typeName = "SCALAR";
+        force = 1;
+    };
+
+    // --medical end
+
 };
